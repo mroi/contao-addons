83e351e72c0dbd3b48dfed7dfaa9b631926967f8
Michael Roitzsch <reactorcontrol@icloud.com>
2019-01-21 18:14:21 +0100

    single slashes are enough

--- config/autoload.php
+++ config/autoload.php
@@ -1,7 +1,7 @@
 <?php
 ClassLoader::addNamespace('Addons');
 ClassLoader::addClasses(array(
-	'Addons\\CSVDownloader' => 'system/modules/addons/classes/CSVDownloader.php',
-	'Addons\\InternalLogMailer' => 'system/modules/addons/classes/InternalLogMailer.php',
-	'Addons\\NewsletterMultimail' => 'system/modules/addons/classes/NewsletterMultimail.php'
+	'Addons\CSVDownloader' => 'system/modules/addons/classes/CSVDownloader.php',
+	'Addons\InternalLogMailer' => 'system/modules/addons/classes/InternalLogMailer.php',
+	'Addons\NewsletterMultimail' => 'system/modules/addons/classes/NewsletterMultimail.php'
 ));
